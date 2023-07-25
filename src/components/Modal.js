2c915f876430391135d8e8c72bea542b71986ea2
MuthuKrishnan27 <muthukrishnan.office@gmail.com>
2023-07-25 12:02:22 +0530

    submitting code-Tue Jul 25 2023 12:02:21 GMT+0530 (India Standard Time)

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const Modal = ({ show, onClose, children }) => {
-  if (!show) {
-    return null;
-  }
+  const modalRef = useRef(null);
 
-  return (
-    <div className="model-overlay">
+  const handleOverlayClick = (event) => {
+    if (event.target === modalRef.current) {
+      onClose();
+    }
+  };
+
+  const handleCloseClick = () => {
+    onClose();
+  };
+
+  return show ? (
+    <div className="model-overlay" ref={modalRef} onClick={handleOverlayClick}>
       <div className="model-content">
-        <button className="model-close" onClick={onClose}>
+        <button className="model-close" onClick={handleCloseClick}>
           Close
         </button>
         <div className="model p">{children}</div>
       </div>
     </div>
-  );
+  ) : null;
 };
 
 export default Modal;
